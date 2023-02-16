1f1d88c621928d4ac1342a0df69e5ce0d0b7bceb
remoteeng00 <remoteeng00@gmail.com>
2023-02-17 02:34:14 +0700

    Add validation rules to SequenceCountHandler

--- config_web/genomics.py
+++ config_web/genomics.py
@@ -23,6 +23,10 @@ APP_LIST_V2 = [
         r"/{pre}/{ver}/prevalence-by-position",
         "web.handlers.v2.genomics.PrevalenceByAAPositionHandler",
     ),
+    (
+        r"/{pre}/{ver}/sequence-count",
+        "web.handlers.v2.genomics.SequenceCountHandler",
+    ),
 ]
 
 APP_LIST_SWITCHED_TO_V2 = [
